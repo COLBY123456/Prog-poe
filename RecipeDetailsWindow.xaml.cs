078a35179296b4301c69edcb3944421d69d73109
COLBY123456 <104758802+COLBY123456@users.noreply.github.com>
2023-07-04 08:29:15 +0200

    Create RecipeDetailsWindow.xaml.cs

---
+++ RecipeDetailsWindow.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+
+namespace RecipeApp
+{
+    public partial class RecipeDetailsWindow : Window
+    {
+        public RecipeDetailsWindow(Recipe recipe)
+        {
+            InitializeComponent();
+            DataContext = recipe;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
